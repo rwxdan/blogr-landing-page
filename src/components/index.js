3a25ef129de36bd79fae6fe98d356b7716f878af
Daniel Castro <dan1elcastro@proton.me>
2022-12-19 17:25:31 -0400

    Add FooterLinks component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Hero from "./Hero";
 import Editor from "./Editor";
 import Phones from "./Phones";
 import Laptop from "./Laptop";
+import FooterLinks from "./FooterLinks";
 
-export { Nav, Hero, Editor, Phones, Laptop };
+export { Nav, Hero, Editor, Phones, Laptop, FooterLinks };
