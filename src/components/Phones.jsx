7a263e62621abbb2d52faba62f36c44c626cdace
Daniel Castro <dan1elcastro@proton.me>
2022-12-19 12:48:20 -0400

    Create Phones component

---
+++ src/components/Phones.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import IllustrationPhones from "/src/assets/static/images/illustration-phones.svg";
+const Phones = () => {
+  return (
+    <section>
+      <div className="article">
+        <div>
+          <img src={IllustrationPhones} alt="A phones " />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Phones;
